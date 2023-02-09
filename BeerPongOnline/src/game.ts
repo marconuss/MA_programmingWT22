cb95578ebc374839b0792444cfe831c55b9d98ba
marconuss <marconuss95@gmail.com>
2023-02-09 15:13:55 +0100

    added game and menu class

---
+++ BeerPongOnline/src/game.ts
@@ -0,0 +1,66 @@
+﻿import {Color3, Mesh, MeshBuilder, Scene, StandardMaterial, Vector3} from "@babylonjs/core";
+import {Room} from "colyseus.js";
+import Menu from "./menu";
+
+
+export default class Game {
+
+    private _scene: Scene;
+
+    private _room: Room;
+    private _playerEntities: { [playerId: string]: Mesh } = {};
+    private _playerStrength: { [playerId: string]: number } = {};
+    private _playerDirection: { [playerId: string]: number } = {};
+
+    constructor(scene: Scene, room: Room) {
+        this._scene = scene;
+        this._room = room;
+    }
+
+    initPlayers(): void {
+        this._room.state.players.onAdd((player, sessionId) => {
+
+            const isCurrentPlayer = (sessionId === this._room.sessionId);
+
+            const sphere = MeshBuilder.CreateSphere(`player-${sessionId}`, {
+                segments: 16,
+                diameter: 0.3
+            }, this._scene);
+
+            const sphereMat = new StandardMaterial(`playerMat-${sessionId}`, this._scene);
+            sphereMat.diffuseColor = new Color3(.9, .9, .9);
+            sphere.material = sphereMat;
+
+            sphere.position = isCurrentPlayer ? sphere.position.set(0, 7, 6.5) : sphere.position.set(0, 7, -6.5);
+
+            this._playerEntities[sessionId] = sphere;
+            this._playerStrength[sessionId] = player.strength;
+            this._playerStrength[sessionId] = player.strength;
+            
+            player.onChange(() => {
+                this._playerStrength[sessionId] = player.strength;
+                this._playerStrength[sessionId] = player.strength;
+            });
+        });
+        
+        this._room.onLeave(() => {
+            this._goToMenu();
+        })
+    }
+    
+    
+    initEnvironment(): void {
+        
+        
+        
+    }
+    
+    private _goToMenu() {
+        const menu = new Menu(this._scene);
+        menu.createMenu();
+    }
+    
+    
+    
+    
+}
---
+++ BeerPongOnline/src/menu.ts
@@ -0,0 +1,39 @@
+﻿import {Scene} from "@babylonjs/core";
+import {AdvancedDynamicTexture, Button} from "@babylonjs/gui";
+import {Client} from "colyseus.js";
+import Game from "./game";
+
+const ROOM_NAME = "Pub";
+const ENDPOINT = "ws://localhost/2567";
+
+export default class Menu{
+    private _scene: Scene;
+    private _advancedTexture: AdvancedDynamicTexture;
+    
+    private _colyseus: Client = new Client(ENDPOINT);
+    
+    constructor(scene: Scene) {
+        this._scene = scene;
+    }
+    
+    public createMenu() : void{
+        
+        this._advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+        const mainBtn = Button.CreateSimpleButton("mainmenu", "START GAME");
+        mainBtn.width = 0.2;
+        mainBtn.height = "40px";
+        mainBtn.color = "white";
+        this._advancedTexture.addControl(mainBtn);
+        //this handles interactions with the start button attached to the scene
+        mainBtn.onPointerUpObservable.add(() => {
+            this._createGame();
+        });
+        
+    }
+    
+    private async _createGame(): Promise<void>{
+        let game: Game;
+    }
+    
+}
